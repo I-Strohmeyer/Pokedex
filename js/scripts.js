65adca588e9911bc20ec75caead9592c1e3a2094
I-Strohmeyer <50380835+I-Strohmeyer@users.noreply.github.com>
2022-03-10 16:44:27 +0100

    added list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = "Pizza";
-
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Scyther", types: [ "Bug", "Flying" ], height: 3, hp: 45, attack: 49, defense: 49 },
+    { name: "Jigglypuff", types: [ "Fairy", "Normal" ], height: 3, hp: 60, attack: 62, defense: 63 },
+    { name: "Dragonite", types: [ "Dragon", "Flying" ], height: 3, hp: 80, attack: 82, defense: 83 },
+    { name: "Charizard", types: [ "Fire", "Flying" ], height: 3, hp: 78, attack: 84, defense: 78 },
+];
